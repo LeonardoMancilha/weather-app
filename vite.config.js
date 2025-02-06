63827def0bf2071e83c78c05ee0c5f4bb00bdadf
Leonardo Machado <leonardomachado.sjc@gmail.com>
2025-02-06 20:24:25 -0300

    tentanto resolver a dependência do react-feather

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    rollupOptions: {
-      external: ['react-feather'],
+    optimizeDeps: {
+      include: ['react-feather'],
+    },
+  },
+  resolve: {
+    alias: {
+      'react-feather': 'react-feather/dist/index.js',
     },
   },
   plugins: [react()],
